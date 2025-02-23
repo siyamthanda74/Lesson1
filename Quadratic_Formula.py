17da2c13cd4dac31c18acccb9234781bef815640
siyamthanda74 <2962032@students.wits.ac.za>
2025-02-23 14:18:53 +0200

    Add files via upload

---
+++ Quadratic_Formula.py
@@ -0,0 +1,8 @@
+a = int(input())
+b = int(input())
+c =int(input())
+print(str(a)+"x**2+"+str(b)+"x+"+ str(c))
+x1 = (-b + (b**2 -4*a*c)**1/2)/2*a
+x2 = (-b - (b**2 -4*a*c)**1/2)/2*a
+print(x1)
+print(x2)
